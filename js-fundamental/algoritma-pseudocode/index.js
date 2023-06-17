7d3e2896ecee06ea19120d3eecd14f719d1a754a
Lasmaditya Ikhwalfebriant <lasmaditya@Administrators-MacBook-Pro-2.local>
2023-06-17 15:51:23 +0700

    Study Case 1 dan Study Case 2

---
+++ js-fundamental/algoritma-pseudocode/index.js
@@ -0,0 +1,40 @@
+// ALGORITMA DAN PESUDOCODE
+
+/**
+ * Study Case 1
+ * Buatlah Algoritma dan Pseudocode untuk luas lingkaran
+ * 
+ * ALGORITMA
+ * 1. Tentukan luasLingkaran: pi * r * r
+ * 2. Buat variabel r
+ * 3. Buat variabel luasLingkaran
+ * 4. Isi luasLingkaran dengan rumus pi kali r kali r
+ * 5. Cetak luaslingkaran
+ * 
+ * PSEUDOCODE
+ * STORE r WITH NUMBER
+ * STORE pi WITH 3.14
+ * STORE luasLlingkaran WITH pi MUL r MUL r
+ * PRINT luasLingkaran
+ * 
+ * 
+ * Study Case 2
+ * Buatlah Algoritma dan Peudocode untuk volume tabung
+ * 
+ * ALGORITMA
+ * 1. Tentukan rumus volumeTabung: pi * r * r * tinggi
+ * 2. Buat variabel r
+ * 3. Buat variabel tinggi
+ * 4. Buat variabel pi
+ * 5. Buat variabel volumeTabung
+ * 6. Isi volumeTabung dengan rumus pi * r * r * tinggi
+ * 7. Cetak volumeTabung
+ * 
+ * PSEUDOCODE
+ * STORE r WITH NUMBER
+ * STORE tinggi WITH NUMBER
+ * STORE pi WITH 3.14
+ * STORE volumeTabung WITH pi MUL r MUL r MUL tinggi
+ * PRINT volumeTabung
+ * 
+ */

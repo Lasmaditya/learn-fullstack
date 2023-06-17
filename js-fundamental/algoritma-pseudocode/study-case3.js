3cfa34289b7f0c234f8aa6c68024e5a0a31028ff
Lasmaditya Ikhwalfebriant <lasmaditya@Administrators-MacBook-Pro-2.local>
2023-06-17 15:59:11 +0700

    Study Case 3

---
+++ js-fundamental/algoritma-pseudocode/study-case3.js
@@ -0,0 +1,20 @@
+// ALGORITMA DAN PSEUDOCODE
+
+/**VOLUME SETENGAH BOLA
+ * 
+ * ALGORITMA
+ * 1. Tentukan rumus VolumeSetengahBola: konstanta * pi * r * r
+ * 2. Buat variabel konstanta
+ * 3. Buat variabel pi
+ * 4. Buat variabel r
+ * 5. Buat variabel VolumeSetengahBola
+ * 6. Isi VolumeSetengahBola dengan rumus 0.67 MUL pu MUL r MUL r
+ * 7. Cetak VolumeSetengahBola
+ * 
+ * PSEUDOCODE
+ * STORE konstanta WITH 0.67
+ * STORE pi WITH 3.14
+ * STORE r WITH NUMBER
+ * STORE VolumeSetengahBola WITH konstanta MUL pi MUL r MUL r
+ * PRINT VolumeSetengahBola 
+ */

d8c426ec2d41582826c7137671ad1f2ccf323fbe
Lasmaditya Ikhwalfebriant <lasmaditya@Administrators-MacBook-Pro-2.local>
2023-06-18 14:53:30 +0700

    Latihan Condition

---
+++ js-fundamental/variabel-tipedata/index2.js
@@ -0,0 +1,16 @@
+var angka = 2;
+if (angka == 1) {
+    console.log('Angka 1');
+} else {
+    console.log('Angka selain 1');
+}
+
+
+var angka = 3;
+if (angka < 2) {
+    console.log('Angka di bawah 2');
+} else if (angka > 2) {
+    console.log('Angka di atas 2');
+} else {
+    console.log('Angka 2')
+}

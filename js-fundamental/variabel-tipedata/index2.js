47cc7a1d13e96d887b4cf028e5d8191cd8df6c49
Lasmaditya Ikhwalfebriant <lasmaditya@Administrators-MacBook-Pro-2.local>
2023-06-18 14:59:53 +0700

    Latihan Condition 3

--- js-fundamental/variabel-tipedata/index2.js
+++ js-fundamental/variabel-tipedata/index2.js
@@ -1,19 +1,19 @@
-// var angka = 2;
-// if (angka == 1) {
-//     console.log('Angka 1');
-// } else {
-//     console.log('Angka selain 1');
-// }
+var angka = 2;
+if (angka == 1) {
+    console.log('Angka 1');
+} else {
+    console.log('Angka selain 1');
+}
 
 
-// var angka = 3;
-// if (angka < 2) {
-//     console.log('Angka di bawah 2');
-// } else if (angka > 2) {
-//     console.log('Angka di atas 2');
-// } else {
-//     console.log('Angka 2')
-// }
+var angka = 3;
+if (angka < 2) {
+    console.log('Angka di bawah 2');
+} else if (angka > 2) {
+    console.log('Angka di atas 2');
+} else {
+    console.log('Angka 2')
+}
 
 var umur = 18;
 if (umur >= 17) {

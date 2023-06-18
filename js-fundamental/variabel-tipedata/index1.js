9e22d31ad6cdb1ce530b6779452cddb44fa44a5a
Lasmaditya Ikhwalfebriant <lasmaditya@Administrators-MacBook-Pro-2.local>
2023-06-18 14:14:07 +0700

    Study Case Terbaru

---
+++ js-fundamental/variabel-tipedata/index1.js
@@ -0,0 +1,62 @@
+//Variable dan Tipe Data
+
+// console.log('Hello World');
+
+//Luas Lingkaran
+var pi = 3.14;
+var r = 11;
+var luas = pi * r * r;
+// console.log(Math.floor(luas));
+
+
+
+//Study Case: Buatlah algoritma, pseudocode, dan codingan keliling persegi panjang
+
+/* Algoritma
+1. Tentukan rumus kelilingPersegiPanjang: (2 * panjang) + (2 * Lebar)
+2. Buat variabel panjang
+2. Buat variabel lebar
+3. Buat variabel kelilingPersegiPanjang
+4. Isi kelilingPersegiPanjang dengan rumus (2 MUL panjang) + (2 MUL lebar)
+5. Cetak kelilingPersegiPanjang
+
+Pseudocode
+STORE panjang WITH number
+STORE lebar WITH number
+STORE kelilingPersegiPanjang WITH (2 MUL panjang) ADD (2 MUL lebar)
+PRINT kelilingPersegiPanjang*/
+
+//Coding
+var panjang = 927;
+var lebar = 64;
+var kelilingPersegiPanjang = (2 * panjang) + (2 * lebar);
+// console.log(Math.floor(kelilingPersegiPanjang));
+
+
+
+//Study Case: Tentukan Algoritma, Pseudocode, dan Codingan dari Luas Permukaan Tabung
+
+/*
+Algoritma
+1. Tentukan rumus luasPermukaanTabung: ((2 * pi * r) * t) + (2 * (pi * r * r))
+2. Buat variabel tinggi
+3. Buat variabel pi
+4. Buat variabel r
+5. Buat variabel luasPermukaanTabung
+6. Isi luasPermukaanTabung dengan rumus ((2 MUL pi MUL r) MUL t) ADD (2 MUL (pi MUL r MUL r))
+7. Cetak luasPermukaanTabung
+
+Pseudocode
+STORE t WITH number
+STORE pi WITH 3.14
+STORE r WITH number
+STORE luasPermukaanTabung WITH ((2 MUL pi MUL r) MUL t) ADD (2 MUL (pi MUL r MUL r))
+PRINT luasPermukaanTabung
+*/
+
+//Coding
+var t = 10;
+var pi = 3.14;
+var r = 5;
+var luasPermukaanTabung = ((2 * pi * r) * t) + (2 * (pi * r * r));
+console.log(Math.floor(luasPermukaanTabung));

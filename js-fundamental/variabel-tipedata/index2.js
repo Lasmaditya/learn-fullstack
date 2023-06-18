73ee7d49419993fbee37f440f8e001574d8ce43d
Lasmaditya Ikhwalfebriant <lasmaditya@Administrators-MacBook-Pro-2.local>
2023-06-18 15:43:02 +0700

    Kondisi Bersarang

--- js-fundamental/variabel-tipedata/index2.js
+++ js-fundamental/variabel-tipedata/index2.js
@@ -15,9 +15,37 @@ if (angka < 2) {
     console.log('Angka 2')
 }
 
+
 var umur = 18;
 if (umur >= 17) {
     console.log('Bisa bikin KTP');
 } else {
     console.log('Belum bisa bikin KTP');
+}
+
+
+var minuman = 'air';
+switch (minuman) {
+    case 'kopi':
+        console.log('Kamu pilih kopi');
+        break;
+    case 'teh':
+        console.log('Kamu pilih teh');
+        break;
+    default:
+        console.log('Kamu tidak minum');
+}
+
+
+var umur = 18;
+var tinggi = 151;
+
+if (umur < 17) {
+    if (tinggi < 150) {
+        console.log('Tidak boleh masuk');
+    } else {
+        console.log('Boleh masuk');
+    }
+} else {
+    console.log('Boleh masuk juga');
 }

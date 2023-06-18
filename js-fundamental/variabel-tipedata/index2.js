d37d533005dbe7ce4e176ac388f7f43919ecbcda
Lasmaditya Ikhwalfebriant <lasmaditya@Administrators-MacBook-Pro-2.local>
2023-06-18 14:57:58 +0700

    Latihan Condition Bikin KTP

--- js-fundamental/variabel-tipedata/index2.js
+++ js-fundamental/variabel-tipedata/index2.js
@@ -1,16 +1,23 @@
-var angka = 2;
-if (angka == 1) {
-    console.log('Angka 1');
-} else {
-    console.log('Angka selain 1');
-}
+// var angka = 2;
+// if (angka == 1) {
+//     console.log('Angka 1');
+// } else {
+//     console.log('Angka selain 1');
+// }
 
 
-var angka = 3;
-if (angka < 2) {
-    console.log('Angka di bawah 2');
-} else if (angka > 2) {
-    console.log('Angka di atas 2');
+// var angka = 3;
+// if (angka < 2) {
+//     console.log('Angka di bawah 2');
+// } else if (angka > 2) {
+//     console.log('Angka di atas 2');
+// } else {
+//     console.log('Angka 2')
+// }
+
+var umur = 18;
+if (umur >= 17) {
+    console.log('Bisa bikin KTP');
 } else {
-    console.log('Angka 2')
+    console.log('Belum bisa bikin KTP');
 }
